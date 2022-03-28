05bdbec7df946ec3b401c1c5f9be2abd76566bc1
wonseok2877 <wonseok2877@gmail.com>
2022-03-29 01:01:24 +0900

    ADD routes MODIFY package app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
 import express from "express";
+import session from "express-session";
+import MySQLStore from "express-mysql-session";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import passport from "passport";
+import dotenv from "dotenv";
+
+import db from "./db";
+import routes from "./routes";
+
+dotenv.config();
 
 const app = express();
 
+// third-party middlewares
 app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MySQLStore({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    }),
+  })
+);
 
 export default app;
