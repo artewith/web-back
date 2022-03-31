5dc86735b30afd17cf69969cea02d9f0c29d1c02
wonseok2877 <wonseok2877@gmail.com>
2022-03-31 22:40:09 +0900

    ADD compression

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
 import passport from "passport";
+import compression from "compression";
 import dotenv from "dotenv";
 
 import pool from "./db";
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
