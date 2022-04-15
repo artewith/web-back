c080027e0887b6b8b8cc66ee9f17ed867100646e
wonseok2877 <wonseok2877@gmail.com>
2022-04-16 00:06:59 +0900

    ADD dev-level naver

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import passport from "passport";
 import { Strategy as KakaoStrategy } from "passport-kakao";
+import { Strategy as NaverStrategy } from "passport-naver-v2";
 import dotenv from "dotenv";
 
 import routes from "../routes";
@@ -9,7 +10,11 @@ import {
   checkIsAuthenticated,
   checkIsNotAuthenticated,
 } from "../middlewares/auth";
-import { logoutController, kakaoCallbackController } from "../controllers/auth";
+import {
+  logoutController,
+  kakaoCallbackController,
+  naverCallbackController,
+} from "../controllers/auth";
 
 dotenv.config();
 
@@ -65,6 +70,59 @@ passport.use(
   )
 );
 
+passport.use(
+  new NaverStrategy(
+    {
+      clientID: process.env.NAVER_KEY,
+      clientSecret: process.env.NAVER_SECRET,
+      callbackURL: routes.AUTH + routes.NAVER_CALLBACK,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      if (profile.email === undefined || profile.name === undefined) {
+        return done(null, false);
+      }
+      const newSession = {
+        provider: profile.provider,
+        id: profile.id,
+      };
+      const connection = await pool.getConnection(async (conn) => conn);
+
+      try {
+        const [result] = await connection.query(
+          "SELECT * FROM users WHERE sns_id = ? AND sns_api_id IN (SELECT id FROM sns_api WHERE name = ?);",
+          [profile.id, profile.provider]
+        );
+        if (result.length) {
+          done(null, { ...newSession, isSuperUser: result[0].is_superuser });
+        } else {
+          await connection.query(
+            "INSERT INTO users (sns_api_id, sns_id, name, email, image_url, gender, age_range, phone_number) VALUES (?,?,?,?,?,?,?,?);",
+            [
+              2,
+              profile.id,
+              profile.name,
+              profile.email,
+              profile.profileImage,
+              profile.gender === "M"
+                ? 1
+                : profile.gender === "F"
+                ? 2
+                : profile.gender === undefined && null,
+              profile.age,
+              profile.mobile,
+            ]
+          );
+        }
+        return done(null, newSession);
+      } catch (error) {
+        return done(error);
+      } finally {
+        connection.release();
+      }
+    }
+  )
+);
+
 router.get(routes.LOGOUT, checkIsAuthenticated, logoutController);
 
 router.get(
@@ -77,6 +135,25 @@ router.get(
   routes.KAKAO_CALLBACK,
   passport.authenticate("kakao"),
   kakaoCallbackController
+);
+
+router.get(
+  routes.NAVER,
+  checkIsNotAuthenticated,
+  passport.authenticate("naver")
+);
+
+router.get(
+  routes.NAVER_FAIL,
+  passport.authenticate("naver", { authType: "reprompt" })
+);
+
+router.get(
+  routes.NAVER_CALLBACK,
+  passport.authenticate("naver", {
+    failureRedirect: routes.AUTH + routes.NAVER_FAIL,
+  }),
+  naverCallbackController
 );
 
 export default router;
