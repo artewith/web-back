72cf4d020428de793768f4061dc1f583ed1a06ca
wonseok2877 <wonseok2877@gmail.com>
2022-05-28 23:12:25 +0900

    Refactor variable names

--- src/controllers/filters.js
+++ src/controllers/filters.js
@@ -4,54 +4,56 @@ import { constants, boxes } from "../utils/filters";
 const { LESSON_ID, TUTOR_ID, ACCOMPANIST_RECRUIT_ID, ACCOMPANIST_RESUME_ID } =
   constants;
 const {
-  education,
-  hasLectured,
-  gender,
-  career,
-  academyWage,
-  accompanyWage,
-  workForm,
-  performerField,
-  price,
+  degrees,
+  hasLectureds,
+  genders,
+  careers,
+  academyWages,
+  accompanyWages,
+  workForms,
+  performerFields,
+  prices,
 } = boxes;
 
 const offerFilters = async (req, res) => {
   const { categoryId } = req.query;
   const connection = await pool.getConnection(async (conn) => conn);
 
-  const [major] = await connection.query(
+  const [majors] = await connection.query(
     "SELECT id AS 'key', name FROM major ORDER BY id;"
   );
-  const [region] = await connection.query(
+  const [regions] = await connection.query(
     "SELECT id AS 'key', name FROM region ORDER BY id;"
   );
-  for (let i = 0; i < region.length; i++) {
-    const [district] = await connection.query(
+  for (let i = 0; i < regions.length; i++) {
+    const [districts] = await connection.query(
       "SELECT id AS 'key', name FROM district WHERE region_id = ? ORDER BY id",
       [i + 1]
     );
-    region[i].district = district;
+    regions[i].districts = districts;
   }
   const numId = Number(categoryId);
 
   connection.release();
 
   const filters = {
-    region,
-    major: numId === LESSON_ID ? major : null,
-    education: numId === LESSON_ID ? education : null,
-    hasLectured: numId === LESSON_ID ? hasLectured : null,
-    gender: numId === LESSON_ID ? gender : null,
-    career:
-      (numId === LESSON_ID) | (numId === ACCOMPANIST_RESUME_ID) ? career : null,
-    wage:
-      numId === TUTOR_ID
-        ? academyWage
-        : (numId === ACCOMPANIST_RECRUIT_ID) | (numId === ACCOMPANIST_RESUME_ID)
-        ? accompanyWage
+    regions,
+    majors: numId === LESSON_ID ? majors : null,
+    degrees: numId === LESSON_ID ? degrees : null,
+    hasLectureds: numId === LESSON_ID ? hasLectureds : null,
+    genders: numId === LESSON_ID ? genders : null,
+    careers:
+      (numId === LESSON_ID) | (numId === ACCOMPANIST_RESUME_ID)
+        ? careers
         : null,
-    workForm: numId === TUTOR_ID ? workForm : null,
-    performerField: numId === ACCOMPANIST_RECRUIT_ID ? performerField : null,
+    wages:
+      numId === TUTOR_ID
+        ? academyWages
+        : (numId === ACCOMPANIST_RECRUIT_ID) | (numId === ACCOMPANIST_RESUME_ID)
+        ? accompanyWages
+        : null,
+    workForms: numId === TUTOR_ID ? workForms : null,
+    performerFields: numId === ACCOMPANIST_RECRUIT_ID ? performerFields : null,
   };
   res.status(200).json(filters);
 };
@@ -59,19 +61,19 @@ const offerFilters = async (req, res) => {
 const practiceHouseFilters = async (req, res) => {
   const connection = await pool.getConnection(async (conn) => conn);
 
-  const [region] = await connection.query(
+  const [regions] = await connection.query(
     "SELECT id AS 'key', name FROM region ORDER BY id;"
   );
-  const [facility] = await connection.query(
+  const [facilities] = await connection.query(
     "SELECT * FROM facility ORDER BY id"
   );
 
   connection.release();
 
   const filters = {
-    region,
-    facility,
-    price,
+    regions,
+    facilities,
+    prices,
   };
 
   res.status(200).json(filters);
@@ -80,31 +82,31 @@ const practiceHouseFilters = async (req, res) => {
 const userFilters = async (req, res) => {
   const connection = await pool.getConnection(async (conn) => conn);
 
-  const [major] = await connection.query(
+  const [majors] = await connection.query(
     `SELECT id AS 'key', name FROM major ORDER BY id`
   );
-  const [job] = await connection.query(
+  const [jobs] = await connection.query(
     `SELECT id AS 'key', name FROM job ORDER BY id`
   );
-  const [region] = await connection.query(
+  const [regions] = await connection.query(
     `SELECT id AS 'key', name FROM region ORDER BY id`
   );
 
-  for (let i = 0; i < region.length; i++) {
-    const [district] = await connection.query(
+  for (let i = 0; i < regions.length; i++) {
+    const [districts] = await connection.query(
       `SELECT id AS 'key', name FROM district WHERE region_id = ? ORDER BY id`,
       [i + 1]
     );
-    region[i].district = district;
+    regions[i].districts = districts;
   }
 
   connection.release();
 
   const filters = {
-    region,
-    major,
-    job,
-    gender,
+    regions,
+    majors,
+    jobs,
+    genders,
   };
 
   res.status(200).json(filters);
